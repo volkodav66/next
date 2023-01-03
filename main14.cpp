07714c6c078638e19cb3cda94d9653533c7bba27
volkodav66 <121555135+volkodav66@users.noreply.github.com>
2023-01-04 01:08:06 +0300

    Add files via upload

---
+++ main14.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+main() 
+{
+	int day;
+	printf("Day?");
+	scanf("%i",&day);
+	
+	switch(day)
+	{
+		case 1:
+			{
+				printf("Monday");
+			}
+			break;
+		case 2:
+			{
+				printf("Tuеsday");
+			}
+			break;	
+		case 3:
+			{
+				printf("Wednesday");
+			}
+			break;	
+		case 4:
+			{
+				printf("Thursday");
+			}
+			break;	
+		case 5:
+			{
+				printf("Friday");
+			}
+			break;	
+		case 6:
+			{
+				printf("Saturday");
+			}
+			break;	
+		case 7:
+			{
+				printf("Sunday");
+			}
+			break;	
+	
+	} 
+}
