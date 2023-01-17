f8577518e37de8e6c3e3b28587e670a2160e87f3
volkodav66 <121555135+volkodav66@users.noreply.github.com>
2023-01-18 00:29:03 +0300

    Update main42.cpp

--- main42.cpp
+++ main42.cpp
@@ -5,11 +5,12 @@ main()
 	int mas1[7];
 	for(int i=0;i<7;i++)
 	{
-	scanf("%i",&mas1[i]);
+		scanf("%i",&mas1[i]);
 	}
+	
 	for(int i=0;i<7;i++)
 	{
-	printf("%4i",mas1[i]);
+		printf("%4i",mas1[i]);
 	}
 	
 	int n;
@@ -19,10 +20,11 @@ main()
 	
 	for(int i=0;i<n;i++)
 	{
-	scanf("%i",&mas2[i]);
+		scanf("%i",&mas2[i]);
 	}
+	
 	for(int i=0;i<n;i++)
 	{
-	printf("%4i",mas2[i]);
+		printf("%4i",mas2[i]);
 	}
 }
