e375cb117efd345142869e2e62d491631d6e88e6
volkodav66 <121555135+volkodav66@users.noreply.github.com>
2023-01-18 00:40:22 +0300

    Update main53.cpp

--- main53.cpp
+++ main53.cpp
@@ -5,10 +5,11 @@
  
 main()
 {
-int mas[100][100];
+	int mas[100][100];
 	int n,m;
 	printf("n=");
 	scanf("%i",&n);
+	
 	while(n<1)
 	{
 		printf("Error. Enter possible number of lines	");
@@ -16,12 +17,14 @@ int mas[100][100];
 	}
 	printf("m=");
 	scanf("%i",&m);
+	
 	while(m<1)
 	{
 		printf("Error. Enter possible number of columns	");
 		scanf("%i",&m);
 	}
 	srand(time(NULL));
+	
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<m;j++)
@@ -29,6 +32,7 @@ int mas[100][100];
 			mas[i][j]=rand()%10+(-2);
 		}
 	}
+	
 	for(int i=0;i<n;i++)
 	{
 		for(int j=0;j<m;j++)
@@ -37,32 +41,4 @@ int mas[100][100];
 		}
 		printf("\n\n");
 	}
-	
-	printf("\n\n\n\n");
-	
-	for(int r=0;r<m;r++)
-	{
-		for(int i=0;i<n;i++)
-		{
-			for(int j=0;j<m-1;j++)
-			{
-				if(mas[i][j]>mas[i][j+1])
-				{
-					int q=mas[i][j];
-					mas[i][j]=mas[i][j+1];
-					mas[i][j+1]=q;
-				}
-			}
-		}
-	}
-	for(int i=0;i<n;i++)
-	{
-		for(int j=0;j<m;j++)
-		{
-			printf("%7i",mas[i][j]);
-		}
-		printf("\n\n");
-	}
-	
-	
 }
