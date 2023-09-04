5d917045763a44f83b3d4a31b05fe0547f93ca19
volkodav66 <121555135+volkodav66@users.noreply.github.com>
2023-09-04 22:48:23 +0300

    Update Ex.2.py

--- 2 semestr/Python/T.3/L.11/Ex.2.py
+++ 2 semestr/Python/T.3/L.11/Ex.2.py
@@ -1,29 +1,65 @@
-import collections
+from collections import deque
+
+pets=dict()
+command=''
 
 def create():
-    ID = collections.deque(pets, maxlen=1)[0]
-    a = input("Имя питомца  ")
-    pets[ID] = pets[a] = {"Вид питомца" : input("Вид питомца   "), "Возраст питомца" : int(input("Возраст питомца  ")), "Имя владельца" : input("Имя владельца ")}
-    return
-def read(ID,a):
-    print("Это", list(pets[ID][a].values())[0], "по кличке", list(pets[ID].keys())[0],". Возраст питомца:", list(pets[ID][a].values())[1], ". Имя владельца", list(pets[ID][a].values())[2])
-    return
+  
+    nickname_=input("Nickname your pet\t")
+    type_=input("Type your pet\t")
+    age_=int(input("Age your pet\t"))
+    name_=input("Name owner\t")
 
-pets={}
-a = input("Имя питомца  ")
-d = int(1)
-pets[d] = pets[a] = {"Вид питомца" : input("Вид питомца   "), "Возраст питомца" : int(input("Возраст питомца  ")), "Имя владельца" : input("Имя владельца ")}
-command = input("Введите команду")
-while (command!='stop'):
-    if command == 'create':
+ 
+
+    pets[nickname_]= {type_,age_,name_}
+
+    
+
+def read():
+    print(pets)
+    return 0
+
+def update():
+
+    nickname_=input()
+    type_=input("Type your pet\t")
+    age_=int(input("Age your pet\t"))
+    name_=input("Name owner\t")
+
+    pets[nickname_]= {type_,age_,name_}
+    return 0
+
+def delete():
+    nickname_=input()
+    del pets[nickname_]
+    return 0
+
+def get_pet(ID):
+
+    return pets[ID] if ID in pets.keys() else False
+
+def get_suffix(age):
+
+    return 0
+
+def pets_list():
+    for nickname_ , age_ in pets:
+        print(f"{nickname_} , {age_}")
+
+    return 0
+
+while(command!='stop'):
+
+    command=input()
+
+    if command=='create':
         create()
-        command = input("Введите команду")
-    if command == 'read':
-        read(int(input("Введите ID вашего питомца")), a)
-        command = input("Введите команду")
-    if command == 'update':
-        update
-        command = input("Введите команду")
-    if command == 'delete':
-        delete
-        command = input("Введите команду")
+    if command=='read':
+        read()
+    if command=='update':
+        update()
+    if command=='delete':
+        delete()
+    if command=='info':
+        pets_list()
